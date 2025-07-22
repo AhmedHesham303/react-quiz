8d80ceea7299c825924fc1957c8bacb8c109aa5c
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-22 16:32:26 +0300

    Displaying progress

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
@@ -40,11 +41,15 @@ function reducer(state, action) {
   }
 }
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
-  const numQestions = questions.length;
+  const numQuestions = questions.length;
+  const maxPossiblePoint = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
@@ -60,10 +65,17 @@ export default function App() {
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
         {status === "ready" && (
-          <StartScreen numQestions={numQestions} dispatch={dispatch} />
+          <StartScreen numQestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "start" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoint={maxPossiblePoint}
+              answer={answer}
+            />
             <Question
               question={questions[index]}
               dispatch={dispatch}
