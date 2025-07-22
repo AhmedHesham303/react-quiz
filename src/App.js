d7c7fc2d82bd5abb07171197b2a79ba6a3db33eb
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-22 12:13:55 +0300

    adding the main useReducer for the quiz app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+const initialState = { questions: [], status: "loading" };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    default:
+      throw new Error("action is unknown");
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => {
+        console.error("Error");
+      });
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,24 +1,25 @@
-import { type } from "@testing-library/user-event/dist/type";
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 function reducer(state, action) {
-  if (action.type === "inc") {
-    return state + 1;
-  }
-  if (action.type === "dec") {
-    return state - 1;
-  }
-  if (action.type === "setCount") {
-    return action.payload;
-  }
+  // if (action.type === "inc") {
+  //   return state + 1;
+  // }
+  // if (action.type === "dec") {
+  //   return state - 1;
+  // }
+  // if (action.type === "setCount") {
+  //   return action.payload;
+  // }
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
+  const initialState = { count: 0, step: 1 };
 
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
+  // const [step, setStep] = useState(1);
   // This mutates the date object.
   const date = new Date(Date.now());
   date.setDate(date.getDate() + count);
@@ -42,12 +43,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
